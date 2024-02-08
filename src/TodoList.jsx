977bab0c2126e21ce8fff75de5ff4f6a53230fdc
Sumanta Ghosh <sg4452095@gmail.com>
2024-02-08 21:17:11 +0530

    Creating Mark as done

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todos, setTodos] = useState([{ task: "sample-task", id: uuidv4() }]);
+  let [todos, setTodos] = useState([
+    { task: "sample-task", id: uuidv4(), isDone: false },
+  ]);
   let [newTodo, setNewTodo] = useState("");
 
   let addNewTask = () => {
     setTodos((prevTodos) => {
-      return [...todos, { task: newTodo, id: uuidv4() }];
+      return [...todos, { task: newTodo, id: uuidv4(), isDone: false }];
     });
     setNewTodo("");
   };
@@ -18,6 +20,62 @@ export default function TodoList() {
 
   let deleteTodo = (id) => {
     setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+  };
+
+  let upperCaseAll = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      })
+    );
+  };
+
+  let upperCaseOne = (id) => {
+    setTodos((prevTask) =>
+      prevTask.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  let markAllDone = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.isDone === false) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  let markAsDone = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
   };
 
   return (
@@ -36,13 +94,30 @@ export default function TodoList() {
       <h4>Todo List</h4>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.task}</span>&nbsp;&nbsp;&nbsp;
+          <li style={{ marginTop: "5px" }} key={todo.id}>
+            <span
+              style={
+                todo.isDone
+                  ? {
+                      textDecoration: "line-through",
+                      fontStyle: "italic",
+                    }
+                  : {}
+              }
+            >
+              {todo.task}
+            </span>
+            &nbsp;&nbsp;&nbsp;
             <button onClick={() => deleteTodo(todo.id)}>delete</button>
+            &nbsp;&nbsp;
+            {/* <button onClick={() => upperCaseOne(todo.id)}>upperCase it</button> */}
+            <button onClick={() => markAsDone(todo.id)}>Mark it as Done</button>
           </li>
         ))}
         &nbsp;&nbsp;
       </ul>
+      <br />
+      <button onClick={markAllDone}>Mark as Done all task</button>
     </div>
   );
 }
