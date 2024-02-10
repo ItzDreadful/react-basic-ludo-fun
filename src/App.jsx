9c44935b3e6cdbc166a7a0d2dd68ecb9a10f4c3b
Sumanta Ghosh <sg4452095@gmail.com>
2024-02-10 21:22:34 +0530

    Create a basic Lottery component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import LudoBoard from "./LudoBoard";
 import TodoList from "./TodoList";
+import Lottery from "./lOTTERY.JSX";
 
 function App() {
   return (
     <>
-      <TodoList />
+      <Lottery />
     </>
   );
 }
